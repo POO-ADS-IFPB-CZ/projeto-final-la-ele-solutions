d91d58ffc01372c3ffc7c80ec14aa1ac17f11274
Lauan Victor <lauanvpn@gmail.com>
2025-03-11 12:18:00 -0300

    Adicionando o código da classe funcionario

--- src/main/java/pessoas/Funcionario.java
+++ src/main/java/pessoas/Funcionario.java
@@ -1,4 +1,41 @@
 package pessoas;
 
-public class Funcionario {
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Funcionario extends Pessoa{
+    private LocalDate dataContratacao;
+    private String cargo;
+    private int cargHoraria;
+    private BigDecimal salario;
+    private String status;
+
+    public Funcionario(String cpf, String nome, char genero, String telefone, String email, LocalDate dataContratacao, String cargo, int cargHoraria, BigDecimal salario, String status) {
+        super(cpf, nome, genero, telefone, email);
+        this.dataContratacao = dataContratacao;
+        this.cargo = cargo;
+        this.cargHoraria = cargHoraria;
+        this.salario = salario;
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Funcionário:\n" +
+                        "  Nome: %s\n" +
+                        "  CPF: %s\n" +
+                        "  Gênero: %c\n" +
+                        "  Telefone: %s\n" +
+                        "  Email: %s\n" +
+                        "  Data de Contratação: %s\n" +
+                        "  Cargo: %s\n" +
+                        "  Carga Horária: %d horas\n" +
+                        "  Salário: R$ %.2f\n" +
+                        "  Status: %s",
+                super.getNome(), super.getCpf(), super.getGenero(),
+                super.getTelefone(), super.getEmail(),
+                dataContratacao, cargo, cargHoraria, salario, status
+        );
+    }
 }
