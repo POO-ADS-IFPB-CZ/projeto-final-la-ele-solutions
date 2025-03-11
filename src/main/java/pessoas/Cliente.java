385adfa31e6c7a4ab2f57a272cb4645107809a2f
cesar.caju <cesar.caju@academico.ifpb.edu.br>
2025-03-11 12:32:07 -0300

    instanciando classe pessoa

--- src/main/java/pessoas/Cliente.java
+++ src/main/java/pessoas/Cliente.java
@@ -1,4 +1,7 @@
 package pessoas;
 
-public class Cliente {
+public class Cliente extends Pessoa{
+    public Cliente(String cpf, String nome, char genero, String telefone, String email) {
+        super(cpf, nome, genero, telefone, email);
+    }
 }
