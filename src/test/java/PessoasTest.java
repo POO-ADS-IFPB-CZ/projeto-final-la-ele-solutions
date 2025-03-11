c4ea85d8fa3970c13f5beccb6cb68e25df6b542c
João Paulo <albuquerque.paulo@academico.ifpb.edu.br>
2025-03-11 16:24:31 -0300

    adicionando mais testes unitários para clientes e funcionários

--- src/test/java/PessoasTest.java
+++ src/test/java/PessoasTest.java
@@ -1,5 +1,6 @@
 import org.junit.Assert;
 import org.junit.Test;
+import pessoas.Cliente;
 import pessoas.Funcionario;
 
 import java.math.BigDecimal;
@@ -25,5 +26,26 @@ public class PessoasTest {
                 "Dentista", 44, BigDecimal.valueOf(2500.00),"Férias");
 
         Assert.assertEquals(resultadoEsperado, funcionarioJoao.toString());
+    }
+    @Test
+    public void deveConseguirInstanciarCliente(){
+        String resultadoEsperado = "Cliente: Pedro - CPF: 18293748532";
+
+        Cliente pedro = new Cliente("18293748532","Pedro",'M',"8299991234", "pedro@email.com");
+
+        Assert.assertEquals(resultadoEsperado, pedro.toString());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void DeveRetornarExceptionAoTentarInstanciarFuncionarioSemNome()
+            throws IllegalArgumentException {
+        Funcionario joaoSemNome = new Funcionario("12345678912", "", 'M', "8399998888", "joao@email.com", LocalDate.of(2025, 3, 11),
+                "Dentista", 44, BigDecimal.valueOf(2500.00),"Férias");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void DeveRetornarExceptionAoTentarInstanciarClienteComEmailInvalido()
+            throws IllegalArgumentException {
+        Cliente Bruna = new Cliente("18234587125", "Bruna da Silva", 'F',"8299991234", "bruna.com");
     }
 }
